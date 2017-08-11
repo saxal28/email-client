ea65174e357cb4b0b66ac6fd3122db10225a3000
Alan Sax <saxal28@gmail.com>
2017-08-10 21:20:18 -0500

    initial

---
+++ index.js
@@ -0,0 +1,14 @@
+var express = require('express')
+var app = express()
+const port = process.env.PORT || 3000;
+
+// respond with "hello world" when a GET request is made to the homepage
+app.get('/', function (req, res) {
+	res.send({
+		test: "this is a test"
+	})
+})
+
+app.listen(port, function () {
+	console.log('Example app listening on port 3000!')
+})
